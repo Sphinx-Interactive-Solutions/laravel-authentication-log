3d2fb3cbbd5c499fd6b1af41326daf0010daaac1
Ayo Adesugba <ayoadesugba@gmail.com>
2023-10-21 13:45:42 -0400

    Update FailedLoginListener.php

--- src/Listeners/FailedLoginListener.php
+++ src/Listeners/FailedLoginListener.php
@@ -3,22 +3,17 @@
 namespace Rappasoft\LaravelAuthenticationLog\Listeners;
 
 use Illuminate\Auth\Events\Failed;
-use Illuminate\Http\Request;
 use Rappasoft\LaravelAuthenticationLog\Notifications\FailedLogin;
 
-class FailedLoginListener
+class FailedLoginListener extends EventListener
 {
-    public Request $request;
-
-    public function __construct(Request $request)
-    {
-        $this->request = $request;
-    }
-
     public function handle($event): void
     {
-        $listener = config('authentication-log.events.failed', Failed::class);
-        if (! $event instanceof $listener) {
+        if (! $this->isListenerForEvent($event, 'failed', Failed::class)) {
+            return;
+        }
+
+        if (! $this->isLoggable($event)) {
             return;
         }
 
